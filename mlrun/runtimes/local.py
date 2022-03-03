fd1da09a543321fee2e138abffa135270ddcced5
tankilevitch <tomtankilevitch@gmail.com>
2022-03-03 16:48:17 +0200

    store each run before updating

--- mlrun/runtimes/local.py
+++ mlrun/runtimes/local.py
@@ -41,7 +41,7 @@ from ..utils.clones import extract_source
 from .base import BaseRuntime
 from .kubejob import KubejobRuntime
 from .remotesparkjob import RemoteSparkRuntime
-from .utils import RunError, global_context, log_std
+from .utils import RunError, global_context, log_std, get_in
 
 
 class ParallelRunner:
@@ -96,6 +96,10 @@ class ParallelRunner:
             resp = client.submit(
                 remote_handler_wrapper, task.to_json(), handler, self.spec.workdir
             )
+            project = get_in(resp, "metadata.project")
+            uid = get_in(resp, "metadata.uid")
+            iter = get_in(resp, "metadata.iteration", 0)
+            mlrun.get_run_db().store_run(task.to_dict(),uid=uid, project=project,itet=iter)
             completed_iter.add(resp)
             queued_runs += 1
             if queued_runs >= parallel_runs:
