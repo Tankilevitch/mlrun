72fe46532764a4673c1fdd46fdcb4980b39a676c
tankilevitch <tomtankilevitch@gmail.com>
2022-10-31 12:15:09 +0200

    add log

--- mlrun/utils/version/version.py
+++ mlrun/utils/version/version.py
@@ -28,6 +28,7 @@ class Version(metaclass=Singleton):
     def __init__(self):
         # When installing un-released version (e.g. by doing pip install git+https://github.com/mlrun/mlrun@development)
         # it won't have a version file, so adding some sane defaults
+        mlrun.utils.logger.info("HIIIIIIIII")
         self.version_info = {"git_commit": "unknown", "version": "0.0.0+unstable"}
         try:
             self.version_info = json.loads(
